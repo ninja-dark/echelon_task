c647dd3d507cf462522f50a118a2d27c2c6578bf
Elena <ninja.dark768@gmail.com>
2023-02-14 21:22:59 +0300

    add test

--- internal/command/command_test.go
+++ internal/command/command_test.go
@@ -6,10 +6,6 @@ import (
 	"github.com/go-playground/assert/v2"
 )
 
-const (
-	testStdoutValue = "test fun value!"
-)
-
 type Want struct {
 	cmd  string
 	args []string
@@ -47,43 +43,3 @@ func TestParseArgs(t *testing.T) {
 		})
 	}
 }
-
-/*
-var mockedExitStatus = 0
-var mockedStdout string
-
-
-func fakeExecCommand(command string, args ...string) *exec.Cmd {
-    cs := []string{"-test.run=TestExecCommandHelper", "--", command}
-    cs = append(cs, args...)
-    cmd := exec.Command(os.Args[0], cs...)
-    es := strconv.Itoa(mockedExitStatus)
-    cmd.Env = []string{"GO_WANT_HELPER_PROCESS=1",
-        "STDOUT=" + mockedStdout,
-        "EXIT_STATUS=" + es}
-    return cmd
-}
-
-func TestExecCommandHelper(t *testing.T) {
-    if os.Getenv("GO_WANT_HELPER_PROCESS") != "1" {
-        return
-    }
-
-    // println("Mocked stdout:", os.Getenv("STDOUT"))
-    fmt.Fprintf(os.Stdout, os.Getenv("STDOUT"))
-    i, _ := strconv.Atoi(os.Getenv("EXIT_STATUS"))
-    os.Exit(i)
-}
-func TestPrintDate(t *testing.T) {
-    mockedExitStatus = 1
-    mockedStdout = "Hello Worl"
-    cmd:= exec.Command(fakeExecCommand)
-    defer func() { cmd = exec.Command }()
-    expDate := "Hello Worl"
-
-    out, _ := printDate()
-    if string(out) != expDate {
-        t.Errorf("Expected %q, got %q", expDate, string(out))
-    }
-}
-*/
